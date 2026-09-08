b58e23f37734851d073f03b1e224539df958f70b
tranquanghai-ops <tranquanghai@tdtu.edu.vn>
2026-09-08 17:54:54 +0700

    Add firebase-config.mjs

---
+++ firebase-config.mjs
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQ-Bvm2cvs4-V3nEvpLoq58GgTx7xzZaE",
+  authDomain: "ifahr-faf5d.firebaseapp.com",
+  projectId: "ifahr-faf5d",
+  storageBucket: "ifahr-faf5d.firebasestorage.app",
+  messagingSenderId: "293718362086",
+  appId: "1:293718362086:web:604c148af6e7ac52deb827"
+};
+
+export const OWNER_EMAIL = "tranquanghai@tdtu.edu.vn";
+export const STUDENT_DOMAIN = "@student.tdtu.edu.vn";
+export const STAFF_DOMAIN = "@tdtu.edu.vn";
